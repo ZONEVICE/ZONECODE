b20627d999a80861549c8a3f0fb8fe3ff9115acc
ZONEVICE <split.vice.dev@gmail.com>
2024-04-24 10:58:19 -0600

    Vue added
    Added Vue SFC props example.

---
+++ vue/props/Whiskey.vue
@@ -0,0 +1,12 @@
+<script setup>
+// Import the file.
+import Zulu from './components/Zulu.vue'
+</script>
+
+<template>
+    <div>
+        <!-- Add the component to virtual DOM. -->
+        <!-- Pass the prop to the component by using its defined property. -->
+        <Zulu message="She likes whiskey. He always carries a nice cold ice flask." />
+    </div>
+</template>
---
+++ vue/props/components/Zulu.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    message: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>Zulu is quite a character!</h1>
+        <h2>{{ message }}</h2>
+        <!-- 
+            Shows on virtual DOM:
+            Zulu is quite a character!
+            She likes whiskey. He always carries a nice cold ice flask.
+        -->
+    </div>
+</template>
