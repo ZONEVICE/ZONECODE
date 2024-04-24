15a3c11406e0d7949336a4d8809acc50b75176b6
ZONEVICE <split.vice.dev@gmail.com>
2024-04-23 20:15:32 -0600

    Golang
    Added switch example.

--- go/fundamentals/switch.go
+++ go/fundamentals/switch.go
@@ -1,1 +1,27 @@
-// todo
+package main
+
+import "fmt"
+
+func main() {
+
+	foo := 1
+	
+	switch foo {
+		case 0:
+			fmt.Println("It is 0")
+		case 1:
+			fmt.Println("It is 1")
+		default:
+			fmt.Println("Dunno what it is")
+	}
+
+	// Weird way.
+	switch bar := 5; bar {
+		case 0:
+			fmt.Println("It is 0")
+		case 1:
+			fmt.Println("It is 1")
+		default:
+			fmt.Println("Dunno what it is")
+	}
+}
