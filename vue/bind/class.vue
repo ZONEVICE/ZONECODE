3da1503000b47e1b48efd85653f53e7e4fc602ea
ZONEVICE <split.vice.dev@gmail.com>
2024-05-20 18:20:11 -0600

    Added Vue 3 bind / class.vue
    Example of Class bind.

---
+++ vue/bind/class.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+// This does not work because this is not a Vue 3 project.
+//import { type Ref, ref } from 'vue'
+
+// Boolean that control the classes to be assigned.
+//const FOO: Ref<boolean> = ref(true)
+//const BAR: Ref<boolean> = ref(true)
+
+// No red underline nor red file alert on vs code.
+const FOO: boolean = true
+const BAR: boolean = true
+
+</script>
+
+<template>
+
+    <!-- Multiple bind class example. -->
+    <!-- https://vuejs.org/guide/essentials/class-and-style.html -->
+    <div :class="[FOO === true ? 'zone' : '', BAR === true ? 'code' : '']">
+        Hello maskeds.
+    </div>
+
+</template>
+
+<style scoped>
+.zone {
+    background-color: black;
+}
+
+.code {
+    color: white;
+}
+</style>
