abc273ddc09ff0aa1bd715608bcdd51052316da7
ZONEVICE <split.vice.dev@gmail.com>
2025-02-28 22:18:41 -0600

    c > variables.c added

---
+++ c/variables.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // -------------------------------------------
+    // integer
+    int foo;
+    foo = 5;
+    int var = 10, zet = 5;
+    printf("The sum is %d\n", foo + var + zet); // 15
+
+    // -------------------------------------------
+    // float
+    float my_float = 100.1234567f;
+    printf("Float is %f\n", my_float);
+
+    // -------------------------------------------
+    // double
+    double my_double = 100.123456;
+    printf("Double is %lf\n", my_double);
+
+    // string
+
+    printf("\n");
+    return 0;
+}
